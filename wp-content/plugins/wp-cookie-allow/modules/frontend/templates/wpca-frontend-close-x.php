36e870fd0ff0819e3eb5649d9ba950d3b02358d4
pabloc97 <pabliyo18@gmail.com>
2022-01-11 12:54:02 +0100

    Add files via upload

---
+++ wp-content/plugins/wp-cookie-allow/modules/frontend/templates/wpca-frontend-close-x.php
@@ -0,0 +1,14 @@
+<svg
+	version="1.1"
+	class=""
+	xmlns="http://www.w3.org/2000/svg"
+	xmlns:xlink="http://www.w3.org/1999/xlink"
+	x="0px"
+	y="0px"
+	width="20px"
+	height="20px"
+	viewBox="0 0 20 20"
+	enable-background="new 0 0 20 20"
+	xml:space="preserve">
+	<polygon fill-rule="evenodd" points="19,3.581 16.42,1 10,7.42 3.581,1 1,3.58 7.42,10 1,16.42 3.581,19 10,12.581 16.42,19 19,16.419 12.581,10" fill="##D7562B"/>
+</svg>
---
+++ wp-content/plugins/wp-cookie-allow/modules/frontend/templates/wpca-frontend-icon-settings.php
@@ -0,0 +1,32 @@
+<svg
+	version="1.1"
+	class=""
+	xmlns="http://www.w3.org/2000/svg"
+	xmlns:xlink="http://www.w3.org/1999/xlink"
+	x="0px"
+	y="0px"
+	width="33px"
+	height="33px"
+	viewBox="0 0 33 33"
+	enable-background="new 0 0 33 33"
+	xml:space="preserve">
+<g>
+	<g>
+		<rect x="11.22" y="3.327" fill="##fcfcfc" width="21.78" height="3.3"/>
+		<rect y="3.327" fill="##fcfcfc" width="7.92" height="3.3"/>
+	</g>
+	<rect x="4.63" fill="##fcfcfc" width="3.3" height="10"/>
+</g>
+<g>
+	<g>
+		<rect x="27.719" y="14.327" fill="##fcfcfc" width="5.281" height="3.3"/>
+		<rect y="14.327" fill="##fcfcfc" width="21.726" height="3.3"/>
+	</g>
+	<rect x="21.118" y="10.977" fill="##fcfcfc" width="3.301" height="10"/>
+</g>
+<g>
+	<rect x="16.06" y="26.35" fill="##fcfcfc" width="16.94" height="3.301"/>
+	<rect y="26.35" fill="##fcfcfc" width="10.065" height="3.301"/>
+	<rect x="9.459" y="23" fill="##fcfcfc" width="3.301" height="10"/>
+</g>
+</svg>
